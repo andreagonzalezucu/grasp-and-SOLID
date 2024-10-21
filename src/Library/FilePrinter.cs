5026754a09dccda1aebbaaa38b6ce93826c82119
andreagonzalezucu <andrea.gonzalezr@correo.ucu.edu.uy>
2024-10-21 08:59:12 -0300

    Cambios para usar DIP

--- src/Library/FilePrinter.cs
+++ src/Library/FilePrinter.cs
@@ -4,9 +4,9 @@ namespace Full_GRASP_And_SOLID
 {
     public class FilePrinter : IPrinter
     {
-        public void PrintRecipe(Recipe recipe)
+        public void PrintRecipe(ITextGenerator textGenerator)
         {
-            File.WriteAllText("Recipe.txt", recipe.GetTextToPrint());
+            File.WriteAllText("Recipe.txt", textGenerator.GetTextToPrint());
         }
     }
 }
